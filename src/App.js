14211ecf85210d6d0ddc39901de80e69cbeb0df5
Abhishek Kumar Mishra <abmis@microsoft.com>
2023-06-15 14:51:12 +0530

    bootstrapped from networkdiagrams-for-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ *  Copyright (c) 2018, The Regents of the University of California,
+ *  through Lawrence Berkeley National Laboratory (subject to receipt
+ *  of any required approvals from the U.S. Dept. of Energy).
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import React, {Component} from 'react';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+import Prism from "prismjs"; // eslint-disable-line
+import "prismjs/components/prism-typescript";
+import "prismjs/themes/prism.css";
+
+// import Intro from "./components/Intro";
+// import Guide from "./components/Guide";
+import Example from "./webcomponents/Example";
+// import API from "./components/API";
+// import Header from './Header';
+// import Sidebar from './Sidebar';
+import ScrollToTop from "./ScrollToTop";
+
+import {bodyStyle, mainStyle, contentStyle} from './styles';
+
+class App extends Component {
+    render() {
+        return(
+            <Router>
+                <div style={bodyStyle}>
+                    {/* <Header /> */}
+                    <Route component={ScrollToTop} />
+                    <div style={mainStyle}>
+                        <div style={contentStyle}>
+                                    {"home "}
+                            <Switch>
+                                {/* <Route exact path="/" component={Intro}/> */}
+                                {/* <Route path="/guide/:doc" component={Guide} /> */}
+                                <Route path="/" component={Example} />
+                                {/* <Route path="/api/:component" component={API} /> */}
+                            </Switch>
+                        </div>
+                        {/* <Sidebar /> */}
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
+
+
